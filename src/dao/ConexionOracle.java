d5ab9ec0511e03174614201ac9cac8b6f66e3d2a
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-27 13:27:59 -0500

    commit - ConexionOracle .

--- src/dao/ConexionOracle.java
+++ src/dao/ConexionOracle.java
@@ -1,4 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionOracle {
-}
+
+    public static Connection getConexion() throws Exception {
+        return DriverManager.getConnection(
+                "jdbc:oracle:thin:@//localhost:1521/orcl",
+                "system",
+                "Tapiero123"
+        );
+    }
+}   

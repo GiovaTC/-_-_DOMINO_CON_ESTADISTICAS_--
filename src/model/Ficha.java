7732ae83824a149e2293e747402a9bf7a2eb8ea0
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-27 12:51:09 -0500

    commit - Ficha .

--- src/model/Ficha.java
+++ src/model/Ficha.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Ficha {
-}
+
+    private int izquierda;
+    private int derecha;
+
+    public Ficha(int izquierda, int derecha) {
+        this.izquierda = izquierda;
+        this.derecha = derecha;
+    }
+
+    public boolean esDoble() {
+        return izquierda == derecha;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +  izquierda + "|" + derecha + "]";
+    }
+}   
